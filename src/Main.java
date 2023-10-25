d3a4e5e751b5f8f9c5bac20ef2e4103b39a1e932
Alexandra Vasileva <a.v.arefeva@gmail.com>
2023-10-25 19:19:25 +0300

    Initial commit

---
+++ src/Main.java
@@ -0,0 +1,8 @@
+public class Main {
+    public static void main(String[] args) {
+        int price = 11250;
+        int bonus = 20;
+        int miles = price / bonus;
+        System.out.println("Начислено бонусов за покупку = " + miles);
+    }
+}
